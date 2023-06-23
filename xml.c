cc375bb11ad95bf056c28c172ea00e863d45fcf9
fcarestiato <114710260+fcarestiato@users.noreply.github.com>
2023-06-23 15:45:53 -0300

    Update xml.c

--- xml.c
+++ xml.c
@@ -1,10 +1,11 @@
+#include "xml.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "consumidor.h"
-#include "listas.h"
 #include "retornos.h"
 #include "jogo_de_tabuleiro.h"
+
 
 int gravaString(FILE* fp,char *string,char *tag){
   int check;
