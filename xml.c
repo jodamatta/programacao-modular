25bbd84d3510d4fd446dec4e680234a807080649
fcarestiato <114710260+fcarestiato@users.noreply.github.com>
2023-06-23 15:44:06 -0300

    Create xml.c

---
+++ xml.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "consumidor.h"
+#include "listas.h"
+#include "retornos.h"
+#include "jogo_de_tabuleiro.h"
+
+int gravaString(FILE* fp,char *string,char *tag){
+  int check;
+  check = fprintf(fp,"  <%s>%s</%s>\n",tag,string,tag);
+  if(check<0){
+    return ERROR_ESCRITA;
+  }
+  return SUCESS;
+}
+int gravaInt(FILE* fp, int valor,char *tag){
+  int check;
+  check = fprintf(fp,"  <%s>%d</%s>\n",tag,valor,tag);
+  if(check<0){
+    return ERROR_ESCRITA;
+  }
+  return SUCESS;
+}
+int gravaFloat(FILE* fp,float valor,char *tag){
+  int check;
+  check = fprintf(fp,"  <%s>%f</%s>\n",tag,valor,tag);
+  if(check<0){
+    return ERROR_ESCRITA;
+  }
+  return SUCESS;
+}
+
+int gravaConsumidor(No* lista){
+  printf("entrou");
+  char* tag;
+  No* aux = lista;
+  printf("oi");
+  int check;
+  InfoConsumidor* info =(InfoConsumidor*)malloc(sizeof(InfoConsumidor));
+  FILE *fp = fopen("consumidor.xml", "w");
+  if (fp == NULL) {
+    printf("Could not open file.\n");
+    return 1;
+  }
+  fprintf(fp,"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+  
+  while(aux != NULL){
+    printf("oi");
+    info = aux->info;
+    fprintf(fp, "<consumidor>\n");
+    tag = "nome";
+    gravaString(fp,info->nome,tag);
+    tag = "sobrenome";
+    gravaString(fp,info->sobrenome,tag);
+    tag = "celular";
+    gravaString(fp,info->celular,tag);
+    tag = "endereco";
+    gravaString(fp,info->endereco,tag);
+    tag = "cpf";
+    gravaString(fp, aux->identificador,tag);
+    aux = aux->prox;
+    fprintf(fp, "</consumidor>\n");
+  }
+}
