7e1964a402b112e13685ace7ae85f1161cb07469
jodamatta <joana.damatta02@gmail.com>
2023-06-23 14:43:01 -0300

    tirei files desnecessárias

--- contatoFornecedor.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONTATO_FORNECEDOR_H
-#define CONTATO_FORNECEDOR_H
-#include "retornos.h"
-
-int enviaJogo(char nome_jogo[50], char nome_arq[55]);
-
-int recebeJogo(char nome_arq[55]);
-
-#endif
