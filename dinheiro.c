2d44b4d0ef88078e5fbf13ca7a77de152061becf
fcarestiato <114710260+fcarestiato@users.noreply.github.com>
2023-06-25 01:48:46 -0300

    Leitura e Gravação XML Saldo

--- dinheiro.c
+++ dinheiro.c
@@ -1,11 +1,12 @@
 #include "listas.h"
 #include "xml.h"
-
+#include "dinheiro.h"
 int avaliaCompra(float preco){
     float saldo;
+    float* ptrSaldo = &saldo;
     float diferenca;
     
-    //saldo = leXML("saldo.xml");
+    lerXMLDinheiro(ptrSaldo);
 
     if(saldo < 0){
         return ERROR_LEITURA;
@@ -17,31 +18,55 @@ int avaliaCompra(float preco){
             return ERROR_COMPRA;
         } 
         else {
-            return SUCCESS;
+            return SUCESS;
         }
     }
 }
 
 int atualizaSaldo(float valor){
     float saldo;
-    int novoSaldo;
+    float* ptrSaldo = &saldo;
+    float novoSaldo;
     int resultado;
-    FILE* saldoXML;
 
-    // ler o saldo da file saldo.xml
+    lerXMLDinheiro(ptrSaldo);
 
     novoSaldo = saldo + valor;
+
 
     if(novoSaldo < 0){
         return ERROR_SALDONEG;
     }
-
+    FILE* saldoXML = fopen("saldo.xml", "w");
+    fprintf(saldoXML, "<dinheiro>\n");
     resultado = gravaFloat(saldoXML, novoSaldo, "saldo");
-
-    if(resultado == SUCCESS){
-        return SUCCESS;
+    fprintf(saldoXML, "</dinheiro>\n");
+    fclose(saldoXML);
+    if(resultado == SUCESS){
+        return SUCESS;
     }
     else {
         return ERROR_ESCRITA;
     }
+}
+
+int lerXMLDinheiro(float* saldo){
+  char linha[100];
+  char *aux;
+  FILE *fp = fopen("saldo.xml", "r");
+  if (fp == NULL) {
+    printf("Could not open file.\n");
+    return ERROR_ARQUIVONEXISTE;
+  }
+  while (fgets(linha, sizeof(linha), fp)) {
+    aux = strstr(linha, "<dinheiro>");
+    if (aux != NULL) {
+      lerDado(fp,linha,9);
+      *saldo = atof(linha);
+      fclose(fp);
+      return SUCESS_LEITURA;
+    }
+  }
+  fclose(fp);
+  return ERROR_LEITURA;
 }
