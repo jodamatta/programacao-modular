1b95df98a25838be1e23c08dff935729236095d3
fcarestiato <114710260+fcarestiato@users.noreply.github.com>
2023-06-25 23:33:24 -0300

    LerFornecedor

--- contato_fornecedor.c
+++ contato_fornecedor.c
@@ -1,5 +1,7 @@
 #include "contato_fornecedor.h"
 #include "xml.h"
+#include "listas.h"
+#include "dinheiro.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -22,4 +24,48 @@ int envia_jogo(char* nome_jogo, int qtd){
             return SUCCESS;
         } 
     }
+}
+
+int lerFornecedor(Lista_jogo* lista){
+  FILE* fp = fopen("arqFornecedor.xml","r");
+  char linha[100];
+  char nome[100];
+  float preco;
+  int qtd;
+  int check;
+  float transacao;
+  InfoJogo * infonova;
+  char *aux;
+  if(fp == NULL){
+    return ERROR_ARQUIVONEXISTE;
+  }
+  while (fgets(linha, sizeof(linha), fp)) {
+    aux = strstr(linha, "  <row>");
+    if (aux != NULL) {
+      fgets(linha,sizeof(linha),fp); // ignora index
+      
+      lerDado(fp,linha,10,8);
+      strcpy(nome,linha);
+      
+      lerDado(fp,linha,11,9);
+      preco = atof(linha);
+      
+      lerDado(fp,linha,16,14);
+      qtd = atoi(linha);
+      
+      transacao = preco*qtd;
+      check = avaliaCompra(transacao);
+      if(check != ERROR_COMPRA){
+        atualizaSaldo(transacao,1);
+        int resultado = busca(lista->inicio, nome, &lista->corrente);
+        if(resultado == ERROR_NAOENCONTRADO){
+          return ERROR_NAOENCONTRADO;
+        }
+        resultado = retorna_info(lista->corrente,&infonova);
+        infonova->qtd_estoque += qtd;
+        atualizacaoJogo(nome,lista,*infonova);
+      } 
+    }
+  }
+  return SUCCESS_LEITURA;
 }
