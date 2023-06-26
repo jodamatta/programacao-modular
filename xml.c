f4ee9d2ade39daa6d84a31381a97da1d08929fa7
fcarestiato <114710260+fcarestiato@users.noreply.github.com>
2023-06-25 22:23:01 -0300

    update funcao lerDado // pq com a identação do fornecedor o tamanho das tags é diferente

--- xml.c
+++ xml.c
@@ -1,10 +1,10 @@
 #include "xml.h"
 
-int lerDado(FILE* fp,char* dado,int tam_tag){
+int lerDado(FILE* fp,char* dado,int tam_tag1,int tam_tag2){
   char linha[100];
   fgets(linha,sizeof(linha),fp);
-  linha[strlen(linha)-tam_tag]='\0';
-  strcpy(dado,linha+tam_tag);
+  linha[strlen(linha)-tam_tag2]='\0';
+  strcpy(dado,linha+tam_tag1);
   return SUCCESS_LEITURA;
 }
 
@@ -34,34 +34,3 @@ int gravaFloat(FILE* fp,float valor,char *tag){
   }
   return SUCCESS;
 }
-
-// int gravaConsumidor(No* lista){
-//   char* tag;
-//   No* aux = lista;
-//   int check;
-//   InfoConsumidor* info =(InfoConsumidor*)malloc(sizeof(InfoConsumidor));
-  
-//   FILE *fp = fopen("consumidor.xml", "w");
-//   if (fp == NULL) {
-//     printf("Could not open file.\n");
-//     return 1;
-//   }
-//   fprintf(fp,"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-  
-//   while(aux != NULL){
-//     info = aux->info;
-//     fprintf(fp, "<consumidor>\n");
-//     tag = "nome";
-//     gravaString(fp,info->nome,tag);
-//     tag = "sobrenome";
-//     gravaString(fp,info->sobrenome,tag);
-//     tag = "celular";
-//     gravaString(fp,info->celular,tag);
-//     tag = "endereco";
-//     gravaString(fp,info->endereco,tag);
-//     tag = "cpf";
-//     gravaString(fp, aux->identificador,tag);
-//     aux = aux->prox;
-//     fprintf(fp, "</consumidor>\n");
-//   }
-// }
