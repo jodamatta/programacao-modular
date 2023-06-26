946c62943095bf13fae44331ab71ecfc941a60bf
fcarestiato <114710260+fcarestiato@users.noreply.github.com>
2023-06-25 23:44:32 -0300

    Update contato_fornecedor.h

--- contato_fornecedor.h
+++ contato_fornecedor.h
@@ -1,7 +1,6 @@
 #ifndef CONTATO_FORNECEDOR_H
 #define CONTATO_FORNECEDOR_H
 #include "retornos.h"
-#include "contato_fornecedor.c"
 #include "jogo_de_tabuleiro.h"
 
 int envia_jogo(char* nome_jogo, int qtd);
